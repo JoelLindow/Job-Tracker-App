3d148026f4d2eb0f890626f6b0bdb7584baf5692
JoelLindow <joellindow@gmail.com>
2017-09-04 19:48:41 -0600

    analysis completed. Used stabby-lambdas for jobs analysis. Code for job-count-by-location is pretty gross but could not solve more eloquently due to time constraints. needs further refactoring

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
 
   def index
+    @jobs_by_interest = Job.by_interest
+    @jobs_top_3 = Job.top_three_companies
+    @jobs_by_city = Job.job_count_by_city
   end
-  
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,8 +4,9 @@ class Job < ApplicationRecord
   belongs_to :category, optional: true
   has_many :comments
 
+  scope :by_interest, -> { all.group_by(&:level_of_interest) }
 
-  # def self.level_of_interest_stats
-  #   (Job.all.group_by(&:level_of_interest))
-  # end
+  scope :top_three_companies, -> { all.group(:company).order("average_level_of_interest desc").average(:level_of_interest).take(3) }
+
+  scope :job_count_by_city, -> { all.group_by(&:city).sort }
 end
